5f363748b9acb9d5b4de440044ae4a994e6b8b89
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-08-29 17:16:15 +0900

    selenium環境もアップする

--- .github/workflows/scripts/start_docker_container.sh
+++ .github/workflows/scripts/start_docker_container.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 set -e
 
+# docker network create selenium-service-network
+
+docker run -d --name selenium-service \
+  -p 4444:4444 \
+  -p 5900:5900 \
+  -p 7900:7900 \
+  --shm-size=2g \
+  -t \
+  --network selenium-service-network \
+  --name chrome \
+  seleniarm/standalone-chromium:114.0
+
+
 cd ~/selenium-service
 docker rm -f selenium-service || true
 docker build -t selenium-service -f ./docker/app/Dockerfile .
 docker run -d --name selenium-service \
   -p 5024:8080 \
+  --network selenium-service-network \
   selenium-service
